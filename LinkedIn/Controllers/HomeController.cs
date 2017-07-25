fbbe95c6cc1fe13b84cf7159a8d0bc183245bfe5
Mukesh Agarwal <smartmuki@gmail.com>
2017-07-25 12:07:51 +0530

    first set

---
+++ LinkedIn/Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkedIn.Controllers
+{
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    public class HomeController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<ActionResult> Index(string code = null)
+        {
+            if (code != null)
+            {
+                var values = new Dictionary<string, string>
+                {
+                    {"grant_type", "authorization_code"},
+                    {"code", code},
+                    {"redirect_uri", "http://localhost:37792"},
+                    {"client_id", "81ufgqsbif2zx5"},
+                    {"client_secret", "xiklDKYAuPTxCDyS"}
+                };
+
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("https://www.linkedin.com/oauth/v2/accessToken", content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                dynamic responseInJson = JObject.Parse(responseString);
+                var accessToken = responseInJson.access_token.ToString();
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+                var profileData =
+                    await
+                        client.GetAsync(
+                            "https://api.linkedin.com/v1/people/~:(id,firstName,lastName,headline,num-connections,picture-url)?format=json");
+                responseString = await profileData.Content.ReadAsStringAsync();
+                responseInJson = JObject.Parse(responseString);
+                ViewBag.firstName = responseInJson.firstName;
+                ViewBag.lastName = responseInJson.lastName;
+                ViewBag.headline = responseInJson.headline;
+                ViewBag.pictureUrl = responseInJson.pictureUrl;
+                ViewBag.name = responseInJson.firstName + " " + responseInJson.lastName;
+            }
+            else
+            {
+                return
+                    Redirect(
+                        "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81ufgqsbif2zx5&redirect_uri=http%3A%2F%2Flocalhost%3A37792&state=DCEeFWf45A53sdfKef424&scope=r_basicprofile");
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ LinkedIn/Scripts/scripts.js
@@ -0,0 +1,69 @@
+// JavaScript source code
+function getOtp() {
+    $("#aadharDiv").hide();
+    $("#otpDiv").show();
+}
+
+function showMainDiv() {
+    $("#firstPage").hide();
+    $("#mainPageOther").show();
+    $("#mainPageOther").css("display", "table");
+}
+
+function beginTimer(timerId) {
+    // Set the date we're counting down to
+    var countDownDate = new Date();
+    countDownDate.setMinutes(countDownDate.getMinutes() + 150);
+    
+    // Update the count down every 1 second
+    var x = setInterval(function () {
+
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Output the result in an element with id="demo"
+        document.getElementById(timerId).innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+        // If the count down is over, write some text 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("demo").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+}
+
+function runProgram() {
+    $("#message").css("display", "inline");
+    document.getElementById("messageText").innerHTML = "Program submitted successfully.";
+}
+
+function compileProgram() {
+    $("#message").css("display", "inline");
+}
+
+function getData(url, params, callbackFunc) {
+    var http = new XMLHttpRequest();
+    //var url = "get_data.php";
+    //var params = "lorem=ipsum&name=binny";
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+            callbackFunc(http.responseText);
+        }
+    }
+
+    http.send(params);
+}
